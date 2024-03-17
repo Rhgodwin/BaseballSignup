a7ccd384f101b0f90d6d468ab9b603558ba59258
Rhgodwin <125306062+Rhgodwin@users.noreply.github.com>
2024-03-17 13:44:08 -0400

    Fixed bug in profile data collection code

--- 1_code/profile.php
+++ 1_code/profile.php
@@ -7,8 +7,8 @@ if (!isset($_SESSION['loggedin'])) {
 	exit;
 }
 $DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'root';
-$DATABASE_PASS = '';
+$DATABASE_USER = 'dataman';
+$DATABASE_PASS = 'data';
 $DATABASE_NAME = 'pwdb';
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if (mysqli_connect_errno()) {
